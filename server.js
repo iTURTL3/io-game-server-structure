ce7cd48d0cdad6f40015c60f8df55ce991c001f8
jakemadness <j4kem4dness@live.com>
2018-04-08 02:12:52 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var port    = 80;//                                     game port
+var port    = 80;//                                     game sockets port
 var sockets = require('socket.io')(port).sockets;//     game sockets
 var game    = require('./includes/game.js')(sockets);// game setup
 game.start();//                                         start game
