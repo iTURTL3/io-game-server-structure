3cf1925ffe2718fb918900b99eee820ee5c61566
jakemadness <j4kem4dness@live.com>
2018-04-08 07:31:44 +0100

    Update game.config.js

--- includes/game.config.js
+++ includes/game.config.js
@@ -7,7 +7,6 @@ module.exports = function() {
    var self             = {};
    self.updateRate      = 1000 / 60;
    self.dayDifference   = 1000 * 60 * 60 / 24;
-   self.dayBonus        = 100;
    self.leaderboardSize = 10;
    return self;
 };
