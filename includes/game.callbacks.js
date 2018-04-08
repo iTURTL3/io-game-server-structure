0599f3a2cb611dcb0506f937a82b04a7755fb4c8
jakemadness <j4kem4dness@live.com>
2018-04-08 01:35:35 +0100

    Create game.callbacks.js

---
+++ includes/game.callbacks.js
@@ -0,0 +1,9 @@
+/*
+********************************************************
+*                     GAME CALLBACKS
+********************************************************
+*/
+module.exports = function(config, data, utilities, mechanics, sockets) {
+   var self = {};
+   return self;
+};
