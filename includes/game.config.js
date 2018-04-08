04915b9a4bc354aa171f088fff8037a62cf6cff8
jakemadness <j4kem4dness@live.com>
2018-04-08 01:20:58 +0100

    Update game.config.js

--- includes/game.config.js
+++ includes/game.config.js
@@ -4,7 +4,10 @@
 ********************************************************
 */
 module.exports = function() {
-   var self     = {};
-   self.version = 1.0;
+   var self             = {};
+   self.updateRate      = 1000 / 60;
+   self.dayDifference   = 1000 * 60 * 60 / 24;
+   self.dayBonus        = 100;
+   self.leaderboardSize = 10;
    return self;
 };
