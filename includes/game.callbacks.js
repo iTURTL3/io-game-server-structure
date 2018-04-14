d11a78e83a9d08bdcba021ade12116754fbd7a3f
jakemadness <j4kem4dness@live.com>
2018-04-14 21:44:23 +0100

    Update game.callbacks.js

--- includes/game.callbacks.js
+++ includes/game.callbacks.js
@@ -4,6 +4,5 @@
 ********************************************************
 */
 module.exports = function(config, data, utilities, mechanics, sockets) {
-   var self = {};
-   return self;
+   var self = this;
 };
