75cbfa720e46ade3b52920c6a4f66ed487d57c7a
jakemadness <j4kem4dness@live.com>
2018-11-18 22:06:15 +0000

    Update game.data.js

--- includes/game.data.js
+++ includes/game.data.js
@@ -6,8 +6,8 @@
 module.exports = function() {
    var self         = this;
    self.serverStart = Date.now();
-   self.loopLast    = Date.now();
-   self.loopNow     = Date.now();
+   self.loopLast    = self.serverStart;
+   self.loopNow     = self.serverStart;
    self.loopDelta   = 1;
    self.gameTime    = [];
    self.leaderboard = [];
